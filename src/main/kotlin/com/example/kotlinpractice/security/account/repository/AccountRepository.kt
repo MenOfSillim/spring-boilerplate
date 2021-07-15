73aa069768fe39bf08d4f3d7260c3f042e96260e
truespring <true_spring_@naver.com>
2021-07-15 11:33:36 +0900

    [feat] remove spring security

--- src/main/kotlin/com/example/kotlinpractice/security/account/repository/AccountRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.kotlinpractice.security.account.repository
-
-import com.example.kotlinpractice.security.account.domain.Account
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AccountRepository : JpaRepository<Account, Long> {
-    fun findByEmail(email: String): Account?
-}
--- src/main/kotlin/com/example/kotlinpractice/util/enumutil/AccountRole.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kotlinpractice.util.enumutil
-
-enum class AccountRole {
-    ADMIN, USER
-}
